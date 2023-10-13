7a88567af1d8fecb13eb89073e9f99fa4c7515ac
DaniSancas <lord.sancas@gmail.com>
2023-10-13 17:09:56 +0200

    add docstrings and minor aesthetic refactor

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+/// Struct to parse command line arguments.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -26,17 +27,15 @@ fn main() {
     if let Some(input_text) = matches.input.as_deref() {
         let lines = input_text.lines();
         for line in lines {
-            if let Some(coincidence) = match_pattern(&regex, &line) {
-                println!("{}", coincidence);
+            if let Some(coincidence) = match_pattern(&regex, line) {
+                println!("{coincidence}");
             }
         }
     } else if let Some(file_path) = matches.file.as_deref() {
         let lines = read_lines(file_path);
-        for line in lines {
-            if let Ok(line_content) = line {
-                if let Some(coincidence) = match_pattern(&regex, &line_content) {
-                    println!("{}", coincidence);
-                }
+        for line in lines.flatten() {
+            if let Some(coincidence) = match_pattern(&regex, &line) {
+                println!("{coincidence}");
             }
         }
     } else {
@@ -44,6 +43,11 @@ fn main() {
     }
 }
 
+/// Function to match a pattern in a line.
+///
+/// Returns the line with the pattern highlighted.
+/// If the pattern is not found, returns None.
+#[must_use]
 pub fn match_pattern(regex: &Regex, line: &str) -> Option<String> {
     if regex.is_match(line) {
         let highlighted = regex.replace_all(line, "\x1b[31m$0\x1b[0m");
@@ -53,7 +57,10 @@ pub fn match_pattern(regex: &Regex, line: &str) -> Option<String> {
     }
 }
 
-/// Function to read lines from a file
+/// Function to read lines from a file.
+///
+/// Returns an iterator over the lines of the file.
+/// If the file cannot be opened, returns an error.
 fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
 where
     P: AsRef<Path>,
